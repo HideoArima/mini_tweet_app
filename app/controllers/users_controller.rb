10e748794e66fce66c8e543c3be40e2cf3f7e6c2
HideoArima <hid727ari@gmail.com>
2020-08-28 14:59:41 +0000

    ６章6.8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
+    redirect_to user_url @user
   end
 end
