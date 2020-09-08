2eb6ff869be9c79e67ef3a182ec8e3213059efa2
HideoArima <hid727ari@gmail.com>
2020-09-08 14:47:14 +0000

    ９章まですべて終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :limitation_correct_user, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -28,11 +29,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.content = params[:content]
     if @post.save
       flash[:notice] = "投稿を編集しました。"
@@ -43,9 +42,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました。"
     redirect_to posts_index_url
   end
+  
+  def limitation_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == @current_user.id
+      flash[:notice] = "自分以外のユーザーの投稿は編集できません。"
+      redirect_to posts_index_url
+    end
+  end  
 end
